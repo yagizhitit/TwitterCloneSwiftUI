431086134f5fbea0edb23fc6f4e2d9871cd9b41a
Yagiz Hitit <yagizhitit@gmail.com>
2023-11-19 16:53:14 +0000

    MainTab Setup

---
+++ TwitterCloneSwiftUI/Core/Messages/MessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesView.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Yağız Hitit on 19.11.2023.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages")
+    }
+}
+
+#Preview {
+    MessagesView()
+}
---
+++ TwitterCloneSwiftUI/Core/Notifications/NotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationsView.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by Yağız Hitit on 19.11.2023.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications")
+    }
+}
+
+#Preview {
+    NotificationsView()
+}
